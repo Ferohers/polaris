26a788741bc00db0b82d68a724331949178582b5
moreal <dev.moreal@gmail.com>
2020-10-11 23:44:39 +0900

    deps: add ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 group = "dev.moreal"
 version = "0.1"
 
+val ktor_version = "1.4.0"
+
 repositories {
     mavenCentral()
     jcenter()
@@ -13,6 +15,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
 
     testImplementation(kotlin("test-junit"))
